debc33705308c25e479f8ee6a88b3da679b736dc
scanner <scanner@JNG.COM>
2026-05-19 11:24:57 +0800

    update routes for dev

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/v2','Home::indexv2');
+// $routes->get('/', 'Home::index');
+
+$routes->get('/main', 'Home::index');
+$routes->get('/main-v2','Home::indexv2');
 $routes->get('/menu','Home::menu');
-$routes->get('/landing-page','Home::landingpage');
-$routes->get('/landing-page-v2','Home::landingpagev2');
+$routes->get('/','Home::landingpage');
+$routes->get('/v2','Home::landingpagev2');
